e8bd77843b2e1ef086bce520c16d6e90614f5931
abarouch <webmaster@meydalle.info>
2019-12-13 15:54:00 +0200

    Insert empty component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content h1').textContent).toContain('chuck-is-funny');
+    expect(compiled.querySelector('h1').textContent).toContain('chuck-is-funny');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { JokesComponent } from './jokes/jokes.component';
+import { MessagesComponent } from './messages/messages.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    JokesComponent,
+    MessagesComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
